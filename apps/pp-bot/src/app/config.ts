c1219972fac760b6ba79165c090dfc82515c6c48
Andrei Serebro <andy.silv@yandex.ru>
2025-07-23 21:48:29 +0100

    [FR-TGBOT-04] Log telegram bot token in useFactory

--- apps/pp-bot/src/app/config.ts
+++ apps/pp-bot/src/app/config.ts
@@ -1,20 +1,25 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TelegrafModuleAsyncOptions } from 'nestjs-telegraf/dist/interfaces';
 import { session } from 'telegraf';
+import { Logger } from '@nestjs/common';
 
 export const TelegrafConfig: TelegrafModuleAsyncOptions = {
   imports: [ConfigModule],
   inject: [ConfigService],
-  useFactory: (cs: ConfigService) => ({
-    middlewares: [session()],
-    options:
-      cs.getOrThrow('TELEGRAM_BOT_TEST_ENV', '0') === '1'
-        ? {
-            telegram: {
-              testEnv: true,
-            },
-          }
-        : {},
-    token: cs.getOrThrow('TELEGRAM_BOT_TOKEN'),
-  }),
+  useFactory: (cs: ConfigService) => {
+    const token = cs.getOrThrow('TELEGRAM_BOT_TOKEN');
+    Logger.log('Obtained TELEGRAM_BOT_TOKEN from ConfigService:', token);
+    return {
+      middlewares: [session()],
+      options:
+        cs.getOrThrow('TELEGRAM_BOT_TEST_ENV', '0') === '1'
+          ? {
+              telegram: {
+                testEnv: true,
+              },
+            }
+          : {},
+      token: token,
+    };
+  },
 };
