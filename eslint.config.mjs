5d420be6a8cf321fe28f998b015343dda36495d9
mvn-h24 <morning.vecher.night@yandex.ru>
2026-01-31 17:11:51 +0300

    refactor: корректирует eslint-правила

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,25 @@
 import nx from '@nx/eslint-plugin';
-import imports from 'eslint-plugin-import-x';
+import perfectionist from 'eslint-plugin-perfectionist';
 
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
-  imports.flatConfigs.typescript,
-  {
-    ignores: ['**/dist'],
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    rules: {
-      "import-x/order": ["error", {
-        "alphabetize": { "order": "asc" },
-        "groups": ["external", "internal", "index", "type"],
-        "newlines-between": "always"
-      }]
-    }
-  },
+  perfectionist.configs['recommended-alphabetical'],
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
       '@nx/enforce-module-boundaries': [
         'error',
         {
-          enforceBuildableLibDependency: true,
           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
           depConstraints: [
             {
-              sourceTag: '*',
               onlyDependOnLibsWithTags: ['*'],
+              sourceTag: '*',
             },
           ],
+          enforceBuildableLibDependency: true,
         },
       ],
     },
