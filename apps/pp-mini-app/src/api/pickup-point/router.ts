0ef147ab90d3281a015d26f4b53dd8da335a6048
mvn-h24 <morning.vecher.night@yandex.ru>
2026-02-02 22:43:29 +0300

    refactor: корректирует API пунктов выдачи

--- apps/pp-mini-app/src/api/pickup-point/router.ts
+++ apps/pp-mini-app/src/api/pickup-point/router.ts
@@ -1,3 +1,5 @@
+import { keys } from 'remeda';
+
 import { createTRPCRouter, employeeProcedure } from '@/server/api/trpc';
 
 import { createRequestSchema, updateRequestSchema } from './schema';
@@ -6,8 +8,17 @@ export const pickupPointEmployeeRouter = createTRPCRouter({
   createOne: employeeProcedure
     .input(createRequestSchema)
     .mutation(async ({ ctx, input }) => {
+      const { projectTasks, ...projectData } = input;
+
       await ctx.db.project.create({
-        data: input,
+        data: {
+          ...projectData,
+          projectTasks: projectTasks?.length
+            ? {
+                createMany: { data: projectTasks },
+              }
+            : undefined,
+        },
       });
     }),
   getList: employeeProcedure.query(async ({ ctx }) =>
@@ -16,13 +27,47 @@ export const pickupPointEmployeeRouter = createTRPCRouter({
   updateOne: employeeProcedure
     .input(updateRequestSchema)
     .mutation(async ({ ctx, input }) => {
-      if (Object.keys(input).length !== 0) {
-        await ctx.db.project.update({
-          data: input,
-          where: {
-            id: input.id,
-          },
-        });
-      }
+      const { id, projectTasks, ...projectData } = input;
+
+      await ctx.db.$transaction(async (tx) => {
+        if (keys(projectData).length > 0) {
+          await tx.project.update({
+            data: projectData,
+            where: { id },
+          });
+        }
+        if (projectTasks && projectTasks.length > 0) {
+          type pt = typeof projectTasks;
+
+          const { tasksToCreate, tasksToUpdate } = projectTasks.reduce(
+            (res, cur) => {
+              if (cur.id === undefined) {
+                res.tasksToCreate.push(cur);
+              } else {
+                res.tasksToUpdate.push(cur);
+              }
+              return res;
+            },
+            { tasksToCreate: [] as pt, tasksToUpdate: [] as pt }
+          );
+
+          for (const task of tasksToUpdate) {
+            const { id: taskId, ...taskData } = task;
+            await tx.project_task.update({
+              data: taskData,
+              where: { id: taskId },
+            });
+          }
+
+          if (tasksToCreate.length > 0) {
+            await tx.project_task.createMany({
+              data: tasksToCreate.map((task) => ({
+                ...task,
+                project_id: id,
+              })),
+            });
+          }
+        }
+      });
     }),
 });
--- apps/pp-mini-app/src/api/pickup-point/schema.ts
+++ apps/pp-mini-app/src/api/pickup-point/schema.ts
@@ -1,12 +1,28 @@
 import { z } from 'zod';
 
-export const createRequestSchema = z.object({
-  // schedule: z.string({ message: 'Необходимо указать Расписание' }).nullable(),
+export const projectTaskSchema = z.object({
+  gender: z.enum(['male', 'female']).nullable().optional(),
+  is_active: z.boolean().default(true),
+  name: z.string({ message: 'Необходимо указать Наименование задачи' }),
+  schedule: z.string({ message: 'Необходимо указать Расписание (RRULE)' }),
+});
+export const projectSchema = z.object({
   address: z.string({ message: 'Необходимо указать Адрес' }).nullable(),
   description: z.string({ message: 'Необходимо указать Описание' }).nullable(),
   name: z.string({ message: 'Необходимо указать Наименовнаие' }),
 });
 
-export const updateRequestSchema = createRequestSchema.partial().extend({
+export const createRequestSchema = projectSchema.extend({
+  projectTasks: z.array(projectTaskSchema).optional(),
+});
+
+export const updateRequestSchema = projectSchema.partial().extend({
   id: z.string(),
+  projectTasks: z
+    .array(
+      projectTaskSchema.partial().extend({
+        id: z.string().optional(),
+      })
+    )
+    .optional(),
 });
